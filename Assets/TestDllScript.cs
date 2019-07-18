744825ffaee750a1d8709f9f829a1428fa8435df
yinyi <yinyi@kuaishou.com>
2019-07-18 08:54:06 +0800

    update

--- Assets/TestDllScript.cs
+++ Assets/TestDllScript.cs
@@ -5,17 +5,18 @@ public class TestDllScript : MonoBehaviour
 {
 	[DllImport("UnityCppInterop")]
 	private static extern int addMethod(int a, int b);
-//	[DllImport("HelloWorld")]
-//	private static extern float initBox2d();
+	[DllImport("HelloWorld")]
+	private static extern float initBox2d();
 
 	[DllImport("UnityCppInterop", EntryPoint = "addMethod")]
 	private static extern int Add(int a, int b);
 	// Use this for initialization
 	void Start () {
 		int a = 5, b = 6;
-//		Debug.LogError("initBox2d : " + initBox2d());
+		Debug.LogError("initBox2d : " + initBox2d());
 		Debug.LogError(string.Format("Internal_Add(): {0} + {1} = {2}", a, b, addMethod(a, b)));
 		Debug.LogError(string.Format("Add(): {0} + {1} = {2}", a, b, Add(a, b)));
+		Debug.LogError("initBox2d : " + initBox2d());
 	}
 	
 	// Update is called once per frame
